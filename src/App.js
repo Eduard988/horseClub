3009758c0d22f424560258bb7bbf5253226157e0
Eduard988 <eduardstepanov14@gmail.com>
2024-05-05 22:14:57 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
+import react from "react";
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <header className="header">
-            <div><img  src='pictur/iconmaksimus.svg' /></div>
-            <button className="aboutUs">О НАС <br /> _____</button>
-            <button className="aboutUs">УСЛУГИ И ЦЕНЫ</button>
-            <button className="aboutUs">ФОТОАЛЬБОМ</button>
-            <button className="aboutUs">КОНТАКТЫ</button>
-            <div>
-                <div><img src="pictur/icontel.png"/></div>
-                <div>+7 (000) 000-00-00</div>
-                <div>Ежедневно 9:00 – 22:00</div>
-            </div>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+
+        </div>
+    );
 }
+
 export default App;
