1a7d973373574d85a10ab14d0ca51768bd637e54
Eduard988 <eduardstepanov14@gmail.com>
2024-05-12 13:54:55 +0300

    body-first-fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react from "react";
 
 
 import './App.css';
+import {ContentSecond} from "./ContentSecond/ContentSecond";
 import {Header} from "./Header/Header";
 import {Footer} from "./Footer/Footer";
 import {ContentFirst} from "./Body/ContentFirst/ContentFirst";
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <ContentFirst/>
+      <ContentSecond />
       <Footer />
     </div>
   );
