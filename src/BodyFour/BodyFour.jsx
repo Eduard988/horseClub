fe1844a3dac2cfff032e8f034950e05d86e8a566
Eduard988 <eduardstepanov14@gmail.com>
2024-05-21 18:55:36 +0300

    body fix

---
+++ src/BodyFour/BodyFour.jsx
@@ -0,0 +1,44 @@
+import s from './BodyFour.module.css'
+
+
+export const BodyFour = () => {
+    return (
+        <div className={s.bodyFour}>
+            {/*<div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={}/>*/}
+            {/*        <h3></h3>*/}
+            {/*        <span></span>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*<button></button>*/}
+        </div>
+    )
+}
